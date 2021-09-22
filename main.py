c2d089adf901b17c2b5c80b3899e8e8f40498623
Gabrielfreitas11 <gabriel.freitas@smarkio.com.br>
2021-09-21 21:04:33 -0300

    fix: digitacao invalidos

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ print("\tComo todo bom líder, você deverá guiar os guerreiros através do lab
 time.sleep(3)
 print("\tBoa Sorte!\n")
 time.sleep(1)
+
+
 def caminho1():
     caminho = {
         "1": "2", 
@@ -72,8 +74,10 @@ def caminho8():
     escolheu = input("\n[1] - Caminho vermelho\n[2] - Caminho preto\n\n")
     return caminho[escolheu]
 
-def  caminho333():
-    print("oi")
+def caminho9():
+    print("\nParabéns!! Você encontrou o fim da Caverna!!")
+    return True
+
 caminhos = {
     "1": caminho1,
     "2": caminho2,
@@ -82,27 +86,25 @@ caminhos = {
     "5": caminho5,
     "6": caminho6,
     "7": caminho7,
-    "8": caminho8
-    }
+    "8": caminho8,
+    "9": caminho9
+}
 
-while True:
+while sala != True:
 
     print("Você está na sala ", sala)
 
-    if sala == "9": 
-        print("\nParabéns!! Você encontrou o fim da Caverna!!")
-        break
-    elif count == 7: 
+    try:  
+        sala = caminhos[sala]()
+    except KeyError:
+        print("\nHmm... O valor que você selecionou parece ser inválido!")
+        print("\nDigite apenas os valores 1 ou 2!\n")    
+        continue
+
+    if count == 7: 
         print("\nHmm... O limite de tentativas foi excedido, tente novamente!")
         break
-    else:
-        print("\nLembre-se de digitar apenas opçãos 1 ou 2")
-        print("1 – Caminho vermelho (ou direita)")
-        print("2 – Caminho preto (ou esquerda)")
-        time.sleep(2)
-
-
-    sala = caminhos[sala]()
-
+        
     count += 1
+
 
